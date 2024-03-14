14b0ca1db203d932d52672ee91a2f51928af4110
Belyakov Nikita <nikitabelekov@gmail.com>
2024-03-14 13:08:13 +0300

    first init

---
+++ stack.go
@@ -0,0 +1,36 @@
+package stackGo
+
+import (
+	"errors"
+	"sync"
+)
+
+type Stack struct {
+	sync.Mutex // Embed the mutex for easier locking
+	s          []interface{}
+}
+
+func NewStack() *Stack {
+	return &Stack{s: make([]interface{}, 0)}
+}
+
+func (s *Stack) Push(v interface{}) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.s = append(s.s, v)
+}
+
+func (s *Stack) Pop() (interface{}, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	l := len(s.s)
+	if l == 0 {
+		return nil, errors.New("empty stack")
+	}
+
+	res := s.s[l-1]
+	s.s = s.s[:l-1]
+	return res, nil
+}
